1cc6e46effa70232debb3e8cbe955e29b66cc504
Jigisha336 <jigishagachuru336@gmail.com>
2026-04-26 20:02:46 +0300

    few updates

--- src/components/Certificates.jsx
+++ src/components/Certificates.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+// eslint-disable-next-line no-unused-vars
 import { motion } from 'framer-motion';
 import { Award, ExternalLink } from 'lucide-react';
 import { Card, CardContent } from './ui/card';
