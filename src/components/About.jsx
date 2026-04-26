f3e5d3d5ae711d3c78b740e9091fa147d02e7b8c
Jigisha336 <jigishagachuru336@gmail.com>
2026-04-26 20:00:19 +0300

    few updates

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -119,7 +119,7 @@ const About = () => {
                 <Badge variant="secondary" className="mx-2">Data Analytics</Badge>
               </motion.span>,{' '}
               <motion.span variants={badgeVariants}>
-                <Badge variant="primary" className="mx-2">Data Engineering</Badge>
+                <Badge variant="secondary" className="mx-2">Data Engineering</Badge>
               </motion.span>, and{' '}
               <motion.span variants={badgeVariants}>
                 <Badge variant="primary" className="mx-2">Backend Development</Badge>
